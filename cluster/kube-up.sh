fb6566b0c9c9a228d5fc1b70d9f82bc275de4528
schwansee <zynlinux@gmail.com>
2016-08-23 21:12:22 +0800

    对kube-up.sh的调用过程做了注释

--- cluster/kube-up.sh
+++ cluster/kube-up.sh
@@ -20,38 +20,53 @@
 # that directly.  If not then we assume we are doing development stuff and take
 # the defaults in the release config.
 
+## 遇到错误，就退出脚本，作用同set -e
 set -o errexit
+## 遇到unset的环境变量就报错退出
 set -o nounset
+## 管道命令失败就返回非0值
 set -o pipefail
 
+## BASH_SOURCE环境变量是一个数组，这里读取数组的第一个元素，也就是执行该脚本时的路径命
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/..
+## ${TXT:-txt} ，这里是${}中:-的用法，即：如果TXT不存在，就用txt字符串代替他
+## 忽略安装中的非致命错误
 EXIT_ON_WEAK_ERROR="${EXIT_ON_WEAK_ERROR:-false}"
 
 if [ -f "${KUBE_ROOT}/cluster/env.sh" ]; then
     source "${KUBE_ROOT}/cluster/env.sh"
 fi
 
+## 引入kube_server_version函数
 source "${KUBE_ROOT}/cluster/kube-env.sh"
+## 引入validate-cluster等函数, 函数的具体实现，是在cluster/KUBERNETES_PROVIDER/util.sh中，它会覆盖原先kube-util.sh中的所有函数
 source "${KUBE_ROOT}/cluster/kube-util.sh"
 
 
+## ${ZONE-}中-的意思是：如果变量ZONE未定义, 则替换为-之后的内容，此处为空；若变量已定义却是空的，也不替换。
 if [ -z "${ZONE-}" ]; then
+  ## KUBERNETES_PROVIDER由kube-env.sh引入, 缺省值为gce
   echo "... Starting cluster using provider: ${KUBERNETES_PROVIDER}" >&2
 else
   echo "... Starting cluster in ${ZONE} using provider ${KUBERNETES_PROVIDER}" >&2
 fi
 
+## 从kube-util.sh中引入,
 echo "... calling verify-prereqs" >&2
 verify-prereqs
 
+## 无脚本引入，所以是看环境变量中是否定义
 if [[ "${KUBE_STAGE_IMAGES:-}" == "true" ]]; then
   echo "... staging images" >&2
+  ## ubuntu/util.sh中的kube-up函数引入了common.sh，其中包含了stage-images函数
   stage-images
 fi
 
+## ubuntu/util.sh中引入了kube-up函数
 echo "... calling kube-up" >&2
 kube-up
 
+## validate-cluster函数从kube-util.sh引入，但没有被util.sh重写，最终将调用validate-cluster.sh脚本
 echo "... calling validate-cluster" >&2
 # Override errexit
 (validate-cluster) && validate_result="$?" || validate_result="$?"
