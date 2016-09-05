3026d25653ae979ca82c84d653cf056b2ce22170
schwansee <zynlinux@gmail.com>
2016-09-05 13:40:33 +0800

    1. 在deploy_env中:增加了DOCKER_REGISTRY_VERSION环境变量 2. 修改install-docker-registry.sh: 从本地导入镜像; 安装/删除docker_registry

--- cluster/install-docker-registry.sh
+++ cluster/install-docker-registry.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
+PACKAGE_PATH=$1
+PACKAGE_PATH=${PACKAGE_PATH:-$HOME}
 
-## To stop registry
-#sudo docker stop registry && docker rm -v registry
+DOCKER_REGISTRY_VERSION=${DOCKER_REGISTRY_VERSION:-2}
+
+## 安装registry
+function install_docker_registry() {
+  ## 先从本地导入registry镜像
+  echo "import registry image"
+  cat ${PACKAGE_PATH}/kubernetes/images/registry-${DOCKER_REGISTRY_VERSION}.tar | sudo docker import - registry:${DOCKER_REGISTRY_VERSION}
+  
+  ## 再运行registry容器
+  sudo docker run -d -p 5000:5000 --restart=always --name registry registry:${DOCKER_REGISTRY_VERSION}
+}
+
+## 删除registry
+function uninstall_docker_registry() {
+  ## 停止、删除registry容器
+  sudo docker stop registry && docker rm -v registry
+
+  ## 删除registry镜像
+  sudo docker rmi register:${DOCKER_REGISTRY_VERSION}
+}
