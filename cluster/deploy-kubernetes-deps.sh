a18969154814ad8b2edb1f9efba8c7a3a56262ea
schwansee <zynlinux@gmail.com>
2016-09-07 10:23:50 +0800

    重命名deploy-kubernetes-deps.sh: 集成了获取依赖包，安装插件的功能

--- cluster/deploy-kubernetes-deps.sh
+++ cluster/deploy-kubernetes-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PACKAGE_PATH=$1
-PACKAGE_PATH=${PACKAGE_PATH:-$HOME}
+DOWNLOAD_TIMEOUT=${DOWNLOAD_TIMEOUT:-60}
+
+PACKAGE_PATH=${PACKAGE_PATH:-$HOME/dashboard_packages}
 
 PAUSE_VERSION=${PAUSE_VERSION:-2.0}
 ETCD_AMD64_VERSION=${ETCD_AMD64_VERSION:-2.2.1}
@@ -10,6 +11,26 @@ SKYDNS_VERSION=${SKYDNS_VERSION:-2015-10-13-8c72f8c}
 EXECHEALTHZ_VERSION=${EXECHEALTHZ_VERSION:-1.0}
 
 DASHBOARD_VERSION=${DASHBOARD_VERSION:-1.0.0}
+
+function get_kubernetes_deps_tar() {
+
+  echo "Package path: ${PACKAGE_PATH}"
+
+  echo "get easy-rsa.tar.gz"
+  if [ ! -f ${PACKAGE_PATH}/easy-rsa.tar.gz ]; then
+    pushd `pwd`
+    cd ${PACKAGE_PATH}
+    curl -L -O https://sstorage.googleapis.com/kubernetes-release/easy-rsa/easy-rsa.tar.gz -m ${DOWNLOAD_TIMEOUT} >& /dev/null
+    if [ $? -ne 0 ]; then
+      echo && echo "easy-rsa download failed"
+      popd
+      exit 110
+    fi
+    popd
+  fi
+
+  echo "TODO: get etcd, flannel & kubernetes packages here"
+}
 
 ## 导入dns插件镜像, 导入顺序根据skydns-rc.yaml中的镜像下载顺序决定
 function import_addons_images_dns() {
