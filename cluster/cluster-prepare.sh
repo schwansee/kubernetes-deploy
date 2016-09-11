d2d84a682cee1b40603e48ea4b2de8b04a8dcf72
schwansee <zynlinux@gmail.com>
2016-09-12 01:14:00 +0800

    1. 完成cluster-prepare.sh的测试 2. 只选择安装部分deploy-docker-images.sh中的镜像

--- cluster/cluster-prepare.sh
+++ cluster/cluster-prepare.sh
@@ -3,13 +3,18 @@
 INSTALL_ROOT=$(dirname "${BASH_SOURCE}")
 
 SCRIPT_PATH=$INSTALL_ROOT/deploy
-ENV_FILE_NAME=deploy_env
+ENV_FILE_NAME=deploy_env 
 source $SCRIPT_PATH/$ENV_FILE_NAME
+roles_array=($roles)
 
+function install_deps() {
 ##scp packages to remote servers
+local ii=0
 for i in $nodes; do
   nodeIP=${i#*@}
-  #scp -r $SCRIPT_PATH $PACKAGE_PATH $nodeIP:$PACKAGE_PATH >& /dev/null
+  echo $nodeIP
+  echo $PACKAGE_PATH
+  #scp -r $SCRIPT_PATH $INSTALL_ROOT/dashboard_packages $nodeIP:$PACKAGE_PATH/../ >& /dev/null
   scp -r $SCRIPT_PATH $nodeIP:$PACKAGE_PATH >& /dev/null
 #  ssh $nodeIP "cd $PACKAGE_PATH/$SCRIPT_PATH && source $ENV_FILE_NAME && \
 #                source deploy-system-hosts.sh && \
@@ -23,7 +28,29 @@ for i in $nodes; do
 #                source deploy-docker-deps.sh && \
 #                install_docker_deps_dpkg"
 #
-  ssh $nodeIP "cd $PACKAGE_PATH/$SCRIPT_PATH && source $ENV_FILE_NAME && \
-                source deploy-docker-images.sh && \
-                load_images_separately"
+
+  if [[ "${roles_array[${ii}]}" == "ai" || "${roles_array[${ii}]}" == "a" ]]; then
+    echo ai or a
+#    ssh $nodeIP "cd $PACKAGE_PATH/$SCRIPT_PATH && source $ENV_FILE_NAME && \
+#                  source deploy-docker-images.sh && \
+#                  load_images_basics && \
+#                  load_images_registry && \
+#                  load_images_heapster && \
+#                  load_images_dns && \
+#                  load_images_dashboard"
+
+  else 
+    echo i
+#    ssh $nodeIP "cd $PACKAGE_PATH/$SCRIPT_PATH && source $ENV_FILE_NAME && \
+#                  source deploy-docker-images.sh && \
+#                  load_images_basics && \
+#                  load_images_registry && \
+#                  load_images_dns"
+
+  fi
+
+  ((ii=ii+1))
 done
+}
+
+install_deps
