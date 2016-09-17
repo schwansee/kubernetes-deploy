9829f8c58e7ddefa870510b4bfcf1215873212f6
schwansee <zynlinux@gmail.com>
2016-09-18 00:12:29 +0800

    改动deploy-kubernetes-deps.sh中安装的组件

--- cluster/deploy/deploy-kubernetes-deps.sh
+++ cluster/deploy/deploy-kubernetes-deps.sh
@@ -5,12 +5,13 @@ DOWNLOAD_TIMEOUT=${DOWNLOAD_TIMEOUT:-60}
 PACKAGE_PATH=${PACKAGE_PATH:-$HOME/dashboard_packages}
 
 PAUSE_VERSION=${PAUSE_VERSION:-2.0}
-ETCD_AMD64_VERSION=${ETCD_AMD64_VERSION:-2.2.1}
-KUBE2SKY_VERSION=${KUBE2SKY_VERSION:-1.14}
-SKYDNS_VERSION=${SKYDNS_VERSION:-2015-10-13-8c72f8c}
-EXECHEALTHZ_VERSION=${EXECHEALTHZ_VERSION:-1.0}
-
-DASHBOARD_VERSION=${DASHBOARD_VERSION:-1.0.0}
+EXECHEALTHZ_AMD64_VERSION=${EXECHEALTHZ_AMD64_VERSION:-1.1}
+KUBEDNS_AMD64_VERSION=${KUBEDNS_AMD64_VERSION:-1.6}
+KUBE_DNSMASQ_AMD64_VERSION=${KUBE_DNSMASQ_AMD64_VERSION:-1.3}
+DASHBOARD_VERSION=${DASHBOARD_VERSION:-1.1.1}
+HEAPSTER_VERSION=${HEAPSTER_VERSION:-canary}
+HEAPSTER_INFLUXDB_VERSION=${HEAPSTER_INFLUXDB_VERSION:-0.5}
+HEAPSTER_GRAFANA_VERSION=${HEAPSTER_GRAFANA_VERSION:-2.6.0-2}
 
 function get_kubernetes_deps_tar() {
 
@@ -72,14 +73,13 @@ function get_kubernetes_deps_tar() {
 
 ## 导入dns插件镜像, 导入顺序根据skydns-rc.yaml中的镜像下载顺序决定
 function import_addons_images_dns() {
-  cat ${PACKAGE_PATH}/kubernetes/images/pause-${PAUSE_VERSION}.tar             | sudo docker import - pause:${PAUSE_VERSION}
-  cat ${PACKAGE_PATH}/kubernetes/images/etcd-amd64-${ETCD_AMD64_VERSION}.tar   | sudo docker import - etcd-amd64:${ETCD_AMD64_VERSION}
-  cat ${PACKAGE_PATH}/kubernetes/images/kube2sky-${KUBE2SKY_VERSION}.tar       | sudo docker import - kube2sky:${KUBE2SKY_VERSION}
-  cat ${PACKAGE_PATH}/kubernetes/images/skydns-${SKYDNS_VERSION}.tar           | sudo docker import - skydns:${SKYDNS_VERSION}
-  cat ${PACKAGE_PATH}/kubernetes/images/exechealthz-${EXECHEALTHZ_VERSION}.tar | sudo docker import - exechealthz:${EXECHEALTHZ_VERSION}
+  sudo docker load < ${PACKAGE_PATH}/kubernetes/images/pause-${PAUSE_VERSION}.tar
+  sudo docker load < ${PACKAGE_PATH}/kubernetes/images/exechealthz-amd64-${EXECHEALTHZ_AMD64_VERSION}.tar
+  sudo docker load < ${PACKAGE_PATH}/kubernetes/images/kubends-amd64-${KUBEDNS_AMD64_VERSION}.tar
+  sudo docker load < ${PACKAGE_PATH}/kubernetes/images/kube-dnsmasq-amd64-${KUBE_DNSMASQ_AMD64_VERSION}.tar
 } 
 
 ## 导入dashboard插件镜像
 function import_addons_images_dashboard() {
-  cat ${PACKAGE_PATH}/kubernetes/images/kubernetes-dashboard-amd64-v${DASHBOARD_VERSION}.tar | sudo docker import - kubernetes-dashboard-amd64:v${EXECHEALTHZ_VERSION}
+  sudo docker load < ${PACKAGE_PATH}/kubernetes/images/kubernetes-dashboard-amd64-v${DASHBOARD_VERSION}.tar
 }
